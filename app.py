80b16550ad1634e53e81932f5c0229b72f6266cb
Pradeep Yadav <104863759+Pradeep9651@users.noreply.github.com>
2024-05-09 18:52:10 +0530

    ggg

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import calendar
 from datetime import datetime
 
 import streamlit as st
-import plotly.graph_objects as go
+#import plotly.graph_objects as go
 
 
 
@@ -39,3 +39,19 @@ with st.form("entry_form", clear_on_submit=True):
     with st.expander("Expenses"):
         for expense in expenses:
             st.number_input(f"{expense}", min_value=0, format="%i", step=10, key=expense)
+    with st.expander("Comment"):
+        comment = st.text_area("", placeholder="Enter a comment here ...")
+
+    "___"
+
+    submitted = st.form_submit_button("Save Data")
+    if submitted:
+        period = str(st.session_state["year"]) + "_" + str(st.session_state["month"])
+        incomes = {income: st.session_state[income] for income in incomes}
+        expenses = {expense: st.session_state[expense] for expense in expenses}
+        #TODO: iNSERT VALUES INTO DATABASE
+
+        st.write(f"incomes: {incomes}")
+        st.write(f"expenses: {expenses}")
+        st.write("Data saved")
+    

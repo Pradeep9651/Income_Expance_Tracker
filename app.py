2cd91f485a45ba8371731dea0f16728522ad9717
Pradeep Yadav <104863759+Pradeep9651@users.noreply.github.com>
2024-05-09 18:06:09 +0530

    fghjk

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+import calendar
+from datetime import datetime
+
 import streamlit as st
 import plotly.graph_objects as go
 
@@ -14,3 +17,15 @@ layout = "centered"
 
 st.set_page_config(page_title=page_title, page_icon=page_icon, layout=layout)
 st.title(page_title + " " + page_icon)
+
+
+years =[datetime.today().year, datetime.today().year + 1]
+months = list(calendar.month_name[1:])
+
+
+
+st.header(f"Data Entry in {currency}")
+with st.form("entry_form", clear_on_submit=True):
+    col1,col2 = st.columns(2)
+    col1.selectbox("Select Month:", months, key="months")
+    col2.selectbox("Select year:", years, key="year")

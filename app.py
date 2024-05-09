f5959a9b367e5ec61d5be724183dc0528413dc11
Pradeep Yadav <104863759+Pradeep9651@users.noreply.github.com>
2024-05-09 17:49:13 +0530

    iet

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import plotly.graph_objects as go
+
+
+
+incomes = ["Salary", "Blog", "Other Income"]
+expenses = ["Rent", "Utilities", "Grocery", "Car", "Other Expenses", "Savings"]
+currency = "USD"
+page_title = ""

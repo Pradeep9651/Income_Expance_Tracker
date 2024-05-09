af5f08bef34cd488aa1f9ad5ba4e3dd61b24c288
Pradeep Yadav <104863759+Pradeep9651@users.noreply.github.com>
2024-05-09 17:53:59 +0530

    kjj

--- app.py
+++ app.py
@@ -6,4 +6,11 @@ import plotly.graph_objects as go
 incomes = ["Salary", "Blog", "Other Income"]
 expenses = ["Rent", "Utilities", "Grocery", "Car", "Other Expenses", "Savings"]
 currency = "USD"
-page_title = ""
+page_title = "Income and Expense Tracker"
+page_icon = ":money_with_wings:"
+layout = "centered"
+
+
+
+st.set_page_config(page_title=page_title, page_icon=page_icon, layout=layout)
+st.title(page_title + " " + page_icon)
